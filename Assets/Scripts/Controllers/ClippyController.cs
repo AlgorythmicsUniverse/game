d3854e2fff6f5706bf4a2902dbf8f032eacbfc38
void <lehelkov35@gmail.com>
2023-04-10 18:10:04 +0300

    Implement code to detect nearby items for later use Refactor parts of code

--- Assets/Scripts/Controllers/ClippyController.cs
+++ Assets/Scripts/Controllers/ClippyController.cs
@@ -6,8 +6,8 @@ public class ClippyController : MonoBehaviour
 {
     // The list of currently picked up game objects.
     private List<GameObject> pickedUp;
-
     private Dictionary<GameObject, float> objectToRotAngle;
+    private List<GameObject> nearbyObjects;
 
     // How many code objects can Clippy pick up?
     [SerializeField]
@@ -25,17 +25,84 @@ public class ClippyController : MonoBehaviour
     [SerializeField]
     public float characterHeight = 1.0f;
 
+    [SerializeField]
+    public float minimumObjectDistance = 5.0f;
+
     void Start() {
         // Instantiate an empty list of picked up items.
         pickedUp = new List<GameObject>();
         objectToRotAngle = new Dictionary<GameObject, float>();
+        nearbyObjects = new List<GameObject>();
     }
 
-    void Update() {
+    void Update() {}
+
+    void FixedUpdate() {
         rotatePickedup();
+        storeNearbyObjects();
     }
 
     void OnTriggerEnter(Collider other) {
+        handleCodeblockTrigger(other);
+    }
+
+    void rotatePickedup() {
+        // Rotate picked up items around Clippy
+        foreach (GameObject obj in pickedUp) {
+            float actualTheta = objectToRotAngle[obj] + (orbitDegreesPerSec * Time.deltaTime);
+            objectToRotAngle[obj] = actualTheta;
+
+            float theta = actualTheta * Mathf.Deg2Rad;
+
+            Vector3 center = transform.position;
+            float x = Mathf.Cos(theta) * orbitDistance + center.x;
+            float y = center.y + characterHeight + (Mathf.Sin(theta) * bobUpAndDownMultiplier);
+            float z = Mathf.Sin(theta) * orbitDistance + center.z;
+
+            obj.transform.position = new Vector3(x, y, z);
+        }
+    }
+    
+    GameObject[] getNearbyObjectsWithTag(string tag, float minimumDistance) {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
+
+        List<GameObject> nearbyObjects = new List<GameObject>();
+        foreach (GameObject obj in objects) {
+            if (Vector3.Distance(transform.position, obj.transform.position) <= minimumDistance) {
+                nearbyObjects.Add(obj);
+            }
+        }
+
+        return nearbyObjects.ToArray();
+    }
+    
+    void storeNearbyObjects() {
+        // Store nearby objects in local list to prevent multiple triggering
+        List<GameObject> objects = new List<GameObject>(getNearbyObjectsWithTag("CodeBlock", minimumObjectDistance));
+        foreach (GameObject obj in objects) {
+            if (!pickedUp.Contains(obj) && !nearbyObjects.Contains(obj)) {
+                nearbyObjects.Add(obj);
+            }
+        }
+
+        // Remove objects no longer nearby
+        foreach (GameObject obj in nearbyObjects.ToArray()) {
+            if (!objects.Contains(obj)) {
+                nearbyObjects.Remove(obj);
+            }
+        }
+    }
+
+    void evenlyDividePickedup() {
+        float step = 360f / pickedUp.Count;
+        int i = 0;
+        foreach (GameObject obj in pickedUp) {
+            objectToRotAngle[obj] = step * i;
+            i++;
+        }
+    }
+
+    void handleCodeblockTrigger(Collider other) {
         GameObject obj = other.gameObject;
 
         if (!obj.CompareTag("Hitbox")) {
@@ -63,6 +130,9 @@ public class ClippyController : MonoBehaviour
         // Pick the object up.
         pickedUp.Add(parentObj);
 
+        // Remove from nearbyObjects list
+        nearbyObjects.Remove(parentObj);
+
         // Initialize rotation for picked up object
         objectToRotAngle[parentObj] = 0;
 
@@ -71,31 +141,5 @@ public class ClippyController : MonoBehaviour
         // Disable the collider of this object's hitbox, since we have picked it up.
         Collider collider = obj.GetComponent<Collider>();
         collider.enabled = false;
-    }
-
-    void rotatePickedup() {
-        // Rotate picked up items around Clippy
-        foreach (GameObject obj in pickedUp) {
-            float actualTheta = objectToRotAngle[obj] + (orbitDegreesPerSec * Time.deltaTime);
-            objectToRotAngle[obj] = actualTheta;
-
-            float theta = actualTheta * Mathf.Deg2Rad;
-
-            Vector3 center = transform.position;
-            float x = Mathf.Cos(theta) * orbitDistance + center.x;
-            float y = center.y + characterHeight + (Mathf.Sin(theta) * bobUpAndDownMultiplier);
-            float z = Mathf.Sin(theta) * orbitDistance + center.z;
-
-            obj.transform.position = new Vector3(x, y, z);
-        }
-    }
-
-    void evenlyDividePickedup() {
-        float step = 360f / pickedUp.Count;
-        int i = 0;
-        foreach (GameObject obj in pickedUp) {
-            objectToRotAngle[obj] = step * i;
-            i++;
-        }
     }
 }
