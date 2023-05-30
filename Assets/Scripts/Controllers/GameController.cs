b57d9fcd5d7bd4fb5ed5d1fadb4d0faef3c605d8
void <lehelkov35@gmail.com>
2023-05-30 16:35:13 +0300

    Rewrite switching between modes

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -6,9 +6,14 @@ public class GameController : MonoBehaviour
 {
     public static GameController Instance = null;
 
+    public float SwitchDuration = 0.2f;
+    public float TabMinScale = 0.2f;
+
     public Camera MainCamera;
     public Canvas GameUI;
+    public Canvas ScreenUI;
     private bool Is3D = true;
+    private bool lockSwitch = false;
 
     void Awake() {
         if (Instance == null) {
@@ -20,35 +25,61 @@ public class GameController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    void Start() {
+        // Initialize camera
+        GameUI.enabled = true;
+        ScreenUI.enabled = false;
+    }
+
     public bool GetIs3D() {
         return Is3D;
     }
 
     public void switchMode() {
+        if (lockSwitch) {
+            return;
+        }
+
         if (Is3D) {
-            to2D();
+            StartCoroutine(to2D());
         } else {
-            to3D();
+            StartCoroutine(to3D());
         }
     }
 
-    public void to2D() {
+    public IEnumerator to2D() {
+        lockSwitch = true;
         Is3D = false;
 
-        MainCamera.orthographic = true;
         MainCamera.GetComponent<Cinemachine.CinemachineBrain>().enabled = false;
         GameUI.enabled = false;
+        ScreenUI.enabled = true;
+        Transform screenGeometry = ScreenUI.transform.Find("Geometry");
+        screenGeometry.transform.localScale = Vector3.one*TabMinScale;
 
+        yield return StartCoroutine(Utility.scaleOverSeconds(screenGeometry, Vector3.one, SwitchDuration));
+
+        MainCamera.orthographic = true;
         Quaternion rotation = Quaternion.Euler(Vector3.zero);
-
         MainCamera.transform.SetPositionAndRotation(Vector3.back, rotation);
+
+        lockSwitch = false;
     }
 
-    public void to3D() {
+    public IEnumerator to3D() {
+        lockSwitch = true;
         Is3D = true;
 
-        MainCamera.orthographic = false;
         MainCamera.GetComponent<Cinemachine.CinemachineBrain>().enabled = true;
+        MainCamera.orthographic = false;
+        Transform screenGeometry = ScreenUI.transform.Find("Geometry");
+        screenGeometry.transform.localScale = Vector3.one;
+
+        yield return StartCoroutine(Utility.scaleOverSeconds(screenGeometry, Vector3.one*TabMinScale, SwitchDuration));
+
         GameUI.enabled = true;
+        ScreenUI.enabled = false;
+        
+        lockSwitch = false;
     }
 }
