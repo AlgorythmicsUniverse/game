0d16ba0b2aeee5faa79579796ee235f680218bcb
void <lehelkov35@gmail.com>
2023-06-11 11:25:25 +0300

    Create global constants class with Chapters list

---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Constants {
+    
+    public static readonly Dictionary<string, string> Chapters = new Dictionary<string, string>() {
+        { "Chapter0", "The Grid"},
+        { "Chapter1", "Fundamentals"},
+        { "Chapter2", "Taking a break"},
+        { "Chapter3", "Fundamentals++"},
+        { "Chapter4", "Going outside"},
+        { "Chapter5", "Beyond borders"},
+        { "Chapter6", "Objects"},
+        { "Chapter7", "The Calculator"},
+        { "Chapter8", "Advanced features"},
+        { "Chapter9", "Algorithms"},
+    };
+}
