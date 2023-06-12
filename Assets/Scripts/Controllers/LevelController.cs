aefee7e1083e68dc089b829f48a005cecdee307c
Kovacs Lehel-Levente <lehelkov35@gmail.com>
2023-06-12 20:33:58 +0300

    Implement Chapters and dummy puzzles (#47)
    Co-authored-by: Bendzsike <bodor.benjamin@outlook.com>

---
+++ Assets/Scripts/Controllers/LevelController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController Instance = null;
+
+    private List<string> unlockedChapters = new List<string>();
+    
+    void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start() {
+        // Unlock tutorial chapter by default
+        unlockChapter("Chapter0");
+    }
+
+    public List<string> getUnlockedChapters() {
+        return unlockedChapters;
+    }
+
+    public void unlockChapter(string chapter) {
+        if (Constants.Chapters.Find(x => x.SceneName == chapter) != null) {
+            if (!this.unlockedChapters.Contains(chapter)) {
+                this.unlockedChapters.Add(chapter);
+            }
+        }
+    }
+
+    public void loadChapter(string chapter) {
+        if (this.unlockedChapters.Contains(chapter)) {
+            SceneManager.LoadScene(chapter, LoadSceneMode.Single);
+        }
+    }
+}
